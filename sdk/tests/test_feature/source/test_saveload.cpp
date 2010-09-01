76bdce2120b43a4cd7b76da149237a65a026c2fb
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2010-09-01 00:35:18 +0000

    The size of script classes and the property offsets are now determined dynamically while loading bytecode instead of reading it from the file itself.
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@678 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/tests/test_feature/source/test_saveload.cpp
+++ sdk/tests/test_feature/source/test_saveload.cpp
@@ -330,7 +330,7 @@ bool Test()
 	mod->SaveByteCode(&stream);
 
 	// TODO: These should eventually be equal, once the bytecode is fully platform independent
-	if( (sizeof(void*) == 4 && stream.buffer.size() != 1445) /* ||
+	if( (sizeof(void*) == 4 && stream.buffer.size() != 1443) /* ||
 		(sizeof(void*) == 8 && stream.buffer.size() != 1616) */ ) 
 	{
 		// Originally this was 3213 (on 32bit)
