f17028ed1c86b35b49f03c8acbd57eaebeb69ee0
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2011-10-23 21:29:28 +0000

    Added a verification in the game sample to make sure the game is executed with the correct working directory, otherwise it would just run without any scripts.
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@1005 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/samples/game/source/main.cpp
+++ sdk/samples/game/source/main.cpp
@@ -25,6 +25,21 @@ int main(int argc, char **argv)
 
 	int r;
 
+	// Make sure the game is being executed with the correct working directory
+	// At the very least there should be a 'player.as' script for controlling the 
+	// player character.
+	FILE *f = fopen("player.as", "r");
+	if( f == 0 )
+	{
+		cout << "The game is not executed in the correct location. Make sure you set the working directory to the path where the 'player.as' script is located." << endl;
+		cout << endl;
+		cout << "Press enter to exit." << endl;
+		char buf[10];
+		cin.getline(buf, 10);
+		return -1;
+	}
+	fclose(f);
+
 	// Initialize the game manager
 	gameMgr = new CGameMgr();
 
