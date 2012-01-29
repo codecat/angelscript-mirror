04fe3769d6f2b306f17400bb6398f8919cee30c6
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2012-01-29 17:37:13 +0000

    Releasing version 2.22.2
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@1135 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/angelscript/include/angelscript.h
+++ sdk/angelscript/include/angelscript.h
@@ -56,7 +56,7 @@ BEGIN_AS_NAMESPACE
 // AngelScript version
 
 #define ANGELSCRIPT_VERSION        22202
-#define ANGELSCRIPT_VERSION_STRING "2.22.2 WIP"
+#define ANGELSCRIPT_VERSION_STRING "2.22.2"
 
 // Data types
 
