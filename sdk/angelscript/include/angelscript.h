6dd686a3d20e987092ebc806edab7e740b166d95
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2012-09-17 23:55:55 +0000

    Releasing 2.25.0
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@1417 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/angelscript/include/angelscript.h
+++ sdk/angelscript/include/angelscript.h
@@ -59,7 +59,7 @@ BEGIN_AS_NAMESPACE
 // AngelScript version
 
 #define ANGELSCRIPT_VERSION        22500
-#define ANGELSCRIPT_VERSION_STRING "2.25.0 WIP"
+#define ANGELSCRIPT_VERSION_STRING "2.25.0"
 
 // Data types
 
