12b38f6aa6cadc019827086a8482a9e78b071196
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2013-07-11 01:44:11 +0000

    Fixed compilation error with has_trivial_destructor on g++ 4.8
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@1663 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/add_on/scripthelper/scripthelper.h
+++ sdk/add_on/scripthelper/scripthelper.h
@@ -39,7 +39,12 @@ template<typename T>
 asUINT GetTypeTraits()
 {
 	bool hasConstructor =  std::is_default_constructible<T>::value && !std::has_trivial_default_constructor<T>::value;
+#if defined(__GNUC__) && __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8) 
+	// http://stackoverflow.com/questions/12702103/writing-code-that-works-when-has-trivial-destructor-is-defined-instead-of-is
+	bool hasDestructor = std::is_destructible<T>::value && !std::is_trivially_destructible<T>::value;
+#else
 	bool hasDestructor = std::is_destructible<T>::value && !std::has_trivial_destructor<T>::value;
+#endif
 	bool hasAssignmentOperator = std::is_copy_assignable<T>::value && !std::has_trivial_copy_assign<T>::value;
 	bool hasCopyConstructor = std::is_copy_constructible<T>::value && !std::has_trivial_copy_constructor<T>::value;
 	bool isFloat = std::is_floating_point<T>::value;
