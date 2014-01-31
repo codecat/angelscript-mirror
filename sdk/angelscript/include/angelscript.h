67d64492e81637a0a8f680549c08233a04d860d6
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2014-01-31 23:34:44 +0000

    Releasing 2.28.1
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@1827 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/angelscript/include/angelscript.h
+++ sdk/angelscript/include/angelscript.h
@@ -59,7 +59,7 @@ BEGIN_AS_NAMESPACE
 // AngelScript version
 
 #define ANGELSCRIPT_VERSION        22801
-#define ANGELSCRIPT_VERSION_STRING "2.28.1 WIP"
+#define ANGELSCRIPT_VERSION_STRING "2.28.1"
 
 // Data types
 
