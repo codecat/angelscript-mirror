2a57b006fbff4d567786916d95211fd7e61f8638
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2015-06-24 01:11:49 +0000

    Fixed compile error in scriptbuilder.cpp
    git-svn-id: http://svn.code.sf.net/p/angelscript/code/trunk@2184 404ce1b2-830e-0410-a2e2-b09542c77caf

--- sdk/add_on/scriptbuilder/scriptbuilder.h
+++ sdk/add_on/scriptbuilder/scriptbuilder.h
@@ -175,7 +175,7 @@ protected:
 	{
 		bool operator()(const std::string &a, const std::string &b) const
 		{
-			return strcmpi(a.c_str(), b.c_str()) < 0;
+			return _strcmpi(a.c_str(), b.c_str()) < 0;
 		}
 	};
 	std::set<std::string, ci_less> includedScripts;
